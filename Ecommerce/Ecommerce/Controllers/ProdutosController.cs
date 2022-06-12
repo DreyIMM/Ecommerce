1a8df97baf170919cda7ae732343a1c88f55fac2
Andrey Dario <andreydario26@gmail.com>
2022-06-12 19:26:03 -0300

    FrontEnd: Ajuste na parte de login

---
+++ Ecommerce/Ecommerce/Controllers/ProdutosController.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Models;
+using Ecommerce.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Controllers
+{
+    public class Produtos : Controller
+    {
+
+        //private readonly IAlunoRepositorio _alunoRepositorio;
+
+        public Produtos()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
