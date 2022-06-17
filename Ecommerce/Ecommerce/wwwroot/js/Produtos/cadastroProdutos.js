b506519d728834d8dccd1d38a16df70977ed1b45
Andrey Dario <andreydario26@gmail.com>
2022-06-17 19:47:12 -0300

    Make Drag and Drop of file in create Product

---
+++ Ecommerce/Ecommerce/wwwroot/js/Produtos/cadastroProdutos.js
@@ -0,0 +1,20 @@
+﻿//Selecionaod a requisão
+
+const DropArea = document.querySelector(".drag-area");
+
+DropArea.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    console.log("File is DragArea");
+    DropArea.classList.add("active");
+})
+
+DropArea.addEventListener("dragleave", () => {
+    console.log("File outside");
+    DropArea.classList.remove("active");
+
+})
+
+DropArea.addEventListener("drop", (event) => {
+    event.preventDefault();
+    console.log("File is dropped on DropArea");
+})
